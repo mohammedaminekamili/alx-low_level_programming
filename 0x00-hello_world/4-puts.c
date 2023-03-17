14403c277b2ad77a2be64efa4f4b1adf857eaf53
mohammedaminekamili <mohamedaninekamili2000@gmail.com>
2023-03-16 17:46:50 -0700

    first c commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-/*first program*/
-int main(void){
+/*
+ * first program
+ * */
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle");
-return 0;
+return (0);
 }
