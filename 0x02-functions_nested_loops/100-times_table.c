5bf1ce1bf1561e76ae3e18ed6f5233e0b91f20c0
mohammedaminekamili <mohamedaninekamili2000@gmail.com>
2023-03-22 18:50:43 -0700

    functions nested loops commit

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table, starting with 0.
+ * @n: The number of the times table to be printed.
+ *
+ * Return: void.
+ */
+void print_times_table(int n)
+{
+int i, j, k;
+if (n > 15 || n < 0)
+return;
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+k = i * j;	
+if (j == 0)
+_putchar('0' + k);
+else if (k < 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+_putchar('0' + k);
+}
+else if (k >= 10 && k < 100)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar('0' + k / 10);
+_putchar('0' + k % 10);
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar('0' + k / 100);
+_putchar('0' + ((k / 10) % 10));
+_putchar('0' + k % 10);
+}
+}
+_putchar('\n');
+}
+}
+
