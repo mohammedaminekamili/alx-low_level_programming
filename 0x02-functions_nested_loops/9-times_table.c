8a72bb1ff5ecfe696576a4c5382d5607677d31c0
mohammedaminekamili <mohamedaninekamili2000@gmail.com>
2023-03-22 18:26:53 -0700

    functions nested loops commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table, starting with 0
+ */
+void times_table(void)
+{
+int row, column, product;
+for (row = 0; row < 10; row++)
+{
+for (column = 0; column < 10; column++)
+{
+product = row * column;
+if (column == 0)
+_putchar('0');
+else if (product < 10)
+{
+_putchar(' ');
+_putchar(product + '0');
+}
+else
+{
+_putchar((product / 10) + '0');
+_putchar((product % 10) + '0');
+}
+if (column < 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
+}
