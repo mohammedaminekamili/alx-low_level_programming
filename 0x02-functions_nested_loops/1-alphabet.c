0cb887c7218c638b97500f2a0a17d158ac4d38b0
mohammedaminekamili <mohamedaninekamili2000@gmail.com>
2023-03-22 17:18:53 -0700

    functions nested loops commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,16 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line
  */
-int main(void)
+void print_alphabet(void)
 {
-    print_alphabet();
-    return (0);
+    char c = 'a';
+
+    while (c <= 'z')
+    {
+        _putchar(c);
+        c++;
+    }
+    _putchar('\n')
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,15 +1,7 @@
-#include #include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
 
+int _putchar(char c);
 void print_alphabet(void);
 
-void print_alphabet(void)
-{
-char letter = 'a';
-
-while (letter <= 'z')
-{
-_putchar(letter);
-letter++;
-}
-_putchar('\n')
-}
+#endif /* MAIN_H */
