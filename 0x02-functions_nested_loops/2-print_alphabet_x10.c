5caf4f8af0e5416ad68df597206921dd46ef0c06
mohammedaminekamili <mohamedaninekamili2000@gmail.com>
2023-03-22 17:37:48 -0700

    functions nested loops commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase,
+ *                      followed by a new line.
+ */
+void print_alphabet_x10(void)
+{
+    int i, j;
+
+    for (i = 0; i < 10; i++)
+    {
+        char letter = 'a';
+
+        while (letter <= 'z')
+        {
+            _putchar(letter);
+            letter++;
+        }
+        _putchar('\n');
+    }
+}
