3e2546550e076321e196e40b2a25ad8b70051b2d
mohammedaminekamili <mohamedaninekamili2000@gmail.com>
2023-03-22 18:41:16 -0700

    functions nested loops commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,16 +8,16 @@
 void print_to_98(int n)
 {
 int i;
-if (n <= 98) 
+if (n <= 98)
 {
-for (i = n; i < 98; i++) 
+for (i = n; i < 98; i++)
 {
 printf("%d, ", i);
 }
-} 
-else 
+}
+else
 {
-for (i = n; i > 98; i--) 
+for (i = n; i > 98; i--)
 {
 printf("%d, ", i);
 }
