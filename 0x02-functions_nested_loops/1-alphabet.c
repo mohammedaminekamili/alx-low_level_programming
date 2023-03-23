6b9bc0f9232cb573233fd4b337cd7531c2a7a06c
mohammedaminekamili <mohamedaninekamili2000@gmail.com>
2023-03-22 17:22:56 -0700

    functions nested loops commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints the alphabet in lowercase, followed by a new line
+ *  main - check the code
+ * Description:print_alphabet - prints the alphabet in lowercase, followed by a new line
+ * Return: Always 0.
  */
 void print_alphabet(void)
 {
