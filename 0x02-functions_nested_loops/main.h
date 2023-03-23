0e99b4f50919a2e610c6f5f5ec8dc63ead5d16e4
mohammedaminekamili <mohamedaninekamili2000@gmail.com>
2023-03-22 17:10:40 -0700

    functions nested loops commit

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#include #include <stdio.h>
+
+void print_alphabet(void);
+
+void print_alphabet(void)
+{
+char letter = 'a';
+
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n')
+}
