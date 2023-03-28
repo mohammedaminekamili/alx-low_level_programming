7438ead8f7fa40633d1cfa2468ebf1478b7eca42
mohammedaminekamili <mohamedaninekamili2000@gmail.com>
2023-03-27 17:42:33 -0700

    commit for alx projects

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,15 +1,26 @@
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string to convert
+ *
+ * Return: the integer value of the string
+ */
+
 int _atoi(char *s)
 {
 int result = 0;
 int sign = 1;
-while (*s != '\0') {
-if (*s == '-') {
+while (*s != '\0')
+{
+if (*s == '-')
+{
 sign = -sign;
 }
-else if (*s >= '0' && *s <= '9') {
+else if (*s >= '0' && *s <= '9')
+{
 result = result * 10 + (*s - '0');
 }
-else if (result != 0) {
+else if (result != 0)
+{
 break;
 }
 s++;
