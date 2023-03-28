585720156836f0ccaebd9451c8d7126547d94e8d
mohammedaminekamili <mohamedaninekamili2000@gmail.com>
2023-03-27 17:28:16 -0700

    commit for alx projects

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by src, including the terminating
+ * null byte, to the buffer pointed to by dest.
+ * @dest: destination buffer
+ * @src: source string
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+char *saved = dest;
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+*dest = '\0';
+return saved;
+}
