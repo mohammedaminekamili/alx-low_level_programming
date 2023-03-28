a6288bdde1948299ee376d83934604ef85ce2450
mohammedaminekamili <mohamedaninekamili2000@gmail.com>
2023-03-27 17:35:08 -0700

    commit for alx projects

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,18 @@
+int _atoi(char *s)
+{
+int result = 0;
+int sign = 1;
+while (*s != '\0') {
+if (*s == '-') {
+sign = -sign;
+}
+else if (*s >= '0' && *s <= '9') {
+result = result * 10 + (*s - '0');
+}
+else if (result != 0) {
+break;
+}
+s++;
+}
+return (result * sign);
+}
